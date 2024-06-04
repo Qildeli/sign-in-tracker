f8d1c2e442a2612ebbd31faeb909e6593820a3b6
qildeli <anaqildeli@gmail.com>
2024-06-04 14:53:19 +0400

    chore: add graphene

---
+++ backend/src/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    sign_in_count = Column(Integer, default=0)
