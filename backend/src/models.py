007112267ea1a097a1efb38b8298aa039d8d5e83
qildeli <anaqildeli@gmail.com>
2024-06-04 23:13:50 +0400

    feat: add global_sign_in_count table

--- backend/src/models.py
+++ backend/src/models.py
@@ -10,3 +10,10 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     sign_in_count = Column(Integer, default=0)
+
+
+class GlobalSignInCount(Base):
+    __tablename__ = "global_sign_in_count"
+
+    id = Column(Integer, primary_key=True, index=True)
+    count = Column(Integer, default=0)
