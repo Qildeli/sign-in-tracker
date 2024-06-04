0a5dc086475870c93f0d888b66966161571cc64f
qildeli <anaqildeli@gmail.com>
2024-06-04 17:48:14 +0400

    refactor: move database setup from main.py to database.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
