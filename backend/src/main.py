3a2f55614660ccaa5d8388ac0bbe06d3bb51e85a
qildeli <anaqildeli@gmail.com>
2024-06-05 00:02:52 +0400

    feat: add GraphQL schema

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,16 @@
+import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
-from database import Base, engine
+from src.database import Base, engine
+from src.graphql.mutations import Mutation
+from src.graphql.queries import Query
 
 Base.metadata.create_all(bind=engine)
 
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+
 app = FastAPI()
+graphql_app = GraphQLRouter(schema)
+
+app.include_router(graphql_app, prefix="/graphql")
