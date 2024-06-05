93972afe70bf7c2eb2205178e0e47313d437fea8
qildeli <anaqildeli@gmail.com>
2024-06-05 15:10:35 +0400

    feat: add init_db function to initialize database with GlobalSignInCount

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
+from src.models import GlobalSignInCount
 from src.settings import SQLALCHEMY_DATABASE_URL
 
 load_dotenv()
@@ -20,3 +21,14 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    db: Session = SessionLocal()
+    if not db.query(GlobalSignInCount).first():
+        global_count = GlobalSignInCount(count=0)
+        db.add(global_count)
+        db.commit()
+
+
+init_db()
