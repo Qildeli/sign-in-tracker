0cdba4d85fd65e7154b6d9e01307c71bfa1d8fce
qildeli <anaqildeli@gmail.com>
2024-06-05 15:30:09 +0400

    refactor: move CORS config to settings

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,30 +1,29 @@
 import strawberry
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from starlette.websockets import WebSocket, WebSocketDisconnect
 from strawberry.fastapi import GraphQLRouter
 
 from src.database import Base, engine
 from src.graphql.mutations import Mutation
 from src.graphql.queries import Query
+from src.settings import add_cors_middleware
 
 Base.metadata.create_all(bind=engine)
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 app = FastAPI()
+
+# Graphql schema
 graphql_app = GraphQLRouter(schema)
 
 app.include_router(graphql_app, prefix="/graphql")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# CORS middleware
+add_cors_middleware(app)
 
+
+# WebSocket
 clients = []
 
 
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,9 +1,29 @@
 import os
 
+from fastapi.middleware.cors import CORSMiddleware
+
+CORS_SETTINGS = {
+    "allow_origins": ["*"],
+    "allow_credentials": True,
+    "allow_methods": ["*"],
+    "allow_headers": ["*"],
+}
+
+
+def add_cors_middleware(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=CORS_SETTINGS["allow_origins"],
+        allow_credentials=CORS_SETTINGS["allow_credentials"],
+        allow_methods=CORS_SETTINGS["allow_methods"],
+        allow_headers=CORS_SETTINGS["allow_headers"],
+    )
+
+
 # database config
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # security config
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = os.getenv('ALGORITHM')
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRATION = int(os.getenv("ACCESS_TOKEN_EXPIRATION"))
