e33690e0c3f60f629a7f4efbf403fd0d2ee51272
qildeli <anaqildeli@gmail.com>
2024-06-07 02:19:29 +0400

    feat: add Register component for user register

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import Button from '@mui/material/Button';
+import Auth from "../layouts/auth";
+import {Box, Grid, TextField} from '@mui/material';
+import Link from "@mui/material/Link";
+import {useMutation} from "@apollo/client";
+import {REGISTER_MUTATION} from "../graphql/mutations";
+import {useNavigate} from "react-router-dom";
+
+
+function Register() {
+    //@ts-ignore
+    const { setToken } = useAuth();
+    const navigate = useNavigate();
+    const [formState, setFormState]: any = useState({})
+
+    const [register] = useMutation(REGISTER_MUTATION, {
+      variables: {
+          input: {
+            email: formState.email,
+            password: formState.password
+          }
+      },
+      onCompleted: ({ register }) => {
+        setToken(register.accessToken);
+        navigate('/dashboard');
+      }
+    });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+
+    setFormState({
+        email, password
+    })
+
+      register()
+  };
+
+
+  return (
+        <Auth title="Sign Up">
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={e => {
+                      setFormState((prevState: any) => ({
+                          ...prevState,
+                          email: e.target.value
+                      }))
+                  }}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  type="password"
+                  name="password"
+                  onChange={p => {
+                      setFormState((prevState: any) => ({
+                          ...prevState,
+                          password: p.target.value
+                      }))
+                  }}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                    Register
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link href="/signin" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+            </Box>
+        </Auth>
+  );
+}
+
+export default Register;
