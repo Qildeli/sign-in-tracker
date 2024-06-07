e5424ce7cd842037d1fc885c0a5ba760f2ee37a0
qildeli <anaqildeli@gmail.com>
2024-06-07 21:59:48 +0400

    feat: add get_context function to extract and decode authorization token

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,27 +1,25 @@
 import strawberry
 from fastapi import FastAPI
-from starlette.websockets import WebSocket
 from strawberry.fastapi import GraphQLRouter
 
 from src.database import Base, engine
 from src.graphql.mutations import Mutation
 from src.graphql.queries import Query
 from src.settings import add_cors_middleware
+from src.utils.context import get_context
 from src.websocket import websocket_endpoint
 
 Base.metadata.create_all(bind=engine)
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
-
 app = FastAPI()
-
-# Graphql schema
-graphql_app = GraphQLRouter(schema)
-
-app.include_router(graphql_app, prefix="/graphql")
 
 # CORS middleware
 add_cors_middleware(app)
 
-# WebSocket route
+# GraphQL router with the schema
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
+app.include_router(graphql_app, prefix="/graphql")
+
+# WebSocket endpoint
 app.websocket("/ws")(websocket_endpoint)
---
+++ backend/src/utils/context.py
@@ -0,0 +1,25 @@
+from fastapi import Request, WebSocket
+from src.utils.auth import decode_token
+
+
+def get_context(request: Request = None, websocket: WebSocket = None):
+    """
+    Extracts the authorization token from the headers of the request,
+    decodes the token to retrieve the user_id and returns it in the context.
+    """
+
+    token = None
+    if request:
+        authorization: str = request.headers.get("Authorization")
+        if authorization:
+            token = authorization.split(" ")[1]
+    elif websocket:
+        authorization: str = websocket.headers.get("Authorization")
+        if authorization:
+            token = authorization.split(" ")[1]
+
+    user_id = None
+    if token:
+        user_id = decode_token(token)
+
+    return {"user_id": user_id}
