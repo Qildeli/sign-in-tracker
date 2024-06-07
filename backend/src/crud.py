dabbb58a7de0221ecaf7d4e8af77bdbce6447e25
qildeli <anaqildeli@gmail.com>
2024-06-07 17:47:27 +0400

    refactor: modularize CRUD operations for user management

---
+++ backend/src/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+
+from src.graphql.types import RegisterInput
+from src.models import User
+from src.utils.auth import get_password_hash
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def create_user(db: Session, user: RegisterInput):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
